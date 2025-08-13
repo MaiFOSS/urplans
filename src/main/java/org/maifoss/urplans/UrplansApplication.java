6fabc5599ce534f72ac5160a4f024e57d0aec05d
aadashti <devoft1@gmail.com>
2025-08-14 00:49:32 +0330

    modified:   data/urplans.mv.db 	modified:   data/urplans.trace.db 	modified:   src/main/java/org/maifoss/urplans/UrplansApplication.java 	modified:   target/classes/org/maifoss/urplans/UrplansApplication.class 	modified:   target/urplans-1.0-SNAPSHOT.jar 	modified:   target/urplans-1.0-SNAPSHOT.jar.original

--- src/main/java/org/maifoss/urplans/UrplansApplication.java
+++ src/main/java/org/maifoss/urplans/UrplansApplication.java
@@ -1,24 +1,11 @@
 package org.maifoss.urplans;
 
-import org.maifoss.urplans.service.TaskService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class UrplansApplication {
     public static void main(String[] args) {
         SpringApplication.run(UrplansApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner seed(TaskService svc) {
-        return args -> {
-            svc.save(new Task(null, "Study Java", "Study to get prepared", LocalDate.of(2025, 8, 13), Task.Priority.URGENT_IMPORTANT));
-            svc.save(new Task(null, "Write README", "Document the project", LocalDate.now(), Task.Priority.NOT_URGENT_IMPORTANT));
-        };
     }
 }
